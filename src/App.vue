047d8f0ff75b40a0112792537da4fd25a0a8f26b
kai-oswald <kaos1910@gmail.com>
2017-06-15 16:17:02 +0200

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,39 @@
 <template>
-  <nav class="nav">
-    <div class="nav-left">
-      <a class="nav-item">
-      mood
-    </a>
-    </div>
+<div id="app">
+  <nav class="nav has-shadow is-fixed">
+    <div class="container">
+      <div class="nav-left">
+        <a href="/" class="nav-item">
+        <img src="/static/img/mood.png"/>
+      </a>
+      </div>
 
-    <div class="nav-center">
-      <a class="nav-item">
-      <span class="icon">
-        <i class="fa fa-github"></i>
-      </span>
-    </a>
-      <a class="nav-item">
-      <span class="icon">
-        <i class="fa fa-twitter"></i>
-      </span>
-    </a>
-    </div>
-
-    <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
-    <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-    <span v-on:click="toggleMenu" class="nav-toggle">
-    <span></span>
-    <span></span>
-    <span></span>
-    </span>
-
-    <!-- This "nav-menu" is hidden on mobile -->
-    <!-- Add the modifier "is-active" to display it on mobile -->
-    <div class="nav-right nav-menu">
-      <a class="nav-item">
-      Home
-    </a>
-      <a class="nav-item">
-      Documentation
-    </a>
-      <a class="nav-item">
-      Blog
-    </a>
-
-      <div class="nav-item">
-        <div class="field is-grouped">
-          <p class="control">
-            <a class="button">
-            <span class="icon">
-              <i class="fa fa-twitter"></i>
-            </span>
-            <span>Tweet</span>
-          </a>
-          </p>
-          <p class="control">
-            <a class="button is-primary">
-            <span class="icon">
-              <i class="fa fa-download"></i>
-            </span>
-            <span>Download</span>
-          </a>
-          </p>
+      <div class="nav-center">
+        <div class="nav-item">
+          <span>mood</span>
         </div>
+      </div>
+
+      <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
+      <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
+      <span v-on:click="toggleMenu" class="nav-toggle">
+    <span></span>
+      <span></span>
+      <span></span>
+      </span>
+
+      <!-- This "nav-menu" is hidden on mobile -->
+      <!-- Add the modifier "is-active" to display it on mobile -->
+      <div class="nav-right nav-menu">
+        <a class="nav-item">
+      <span class="icon"><i class="fa fa-github" aria-hidden="true"></i></span>
+      <span>github</span>    
+    </a>
       </div>
     </div>
   </nav>
+  <router-view></router-view>
+  </div>
 </template>
 
 <script>
@@ -84,8 +56,8 @@
 </script>
 
 <style>
-/*
-  @import url('/node_modules/bulma/css/bulma.css');
-  @import url('/node_modules/font-awesome/css/font-awesome.min.css');
-*/
+.is-fixed {
+  position: fixed;
+  width: 100%;
+}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero is-light is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <img src="/static/img/mood.png" />       
+        <div class="columns">
+          <div class="column is-half is-offset-one-quarter">
+            <div class="field">
+              <p class="control">
+                <input class="input" type="text" placeholder="@realdonaldtrump">
+              </p>
+            </div>
+            <a class="button is-primary is-outlined is-fullwidth">Get in the mood</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {}
+
+</script>
+<style scoped>
+
+
+</style>
