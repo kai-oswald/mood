ddf34dccdd954e62e574814ee962e36895b52476
kai-oswald <kaos1910@gmail.com>
2017-06-23 21:58:38 +0200

    reduce to minimum required components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,5 @@
 <template>
 <div id="app">
-  <nav class="nav has-shadow is-fixed">
-    <div class="container">
-      <div class="nav-left">
-        <a href="/" class="nav-item">
-        <img src="/static/img/mood.png"/>
-      </a>
-      </div>
-
-      <div class="nav-center">
-        <div class="nav-item">
-          <span>mood</span>
-        </div>
-      </div>
-
-      <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
-      <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-      <span v-on:click="toggleMenu" class="nav-toggle">
-    <span></span>
-      <span></span>
-      <span></span>
-      </span>
-
-      <!-- This "nav-menu" is hidden on mobile -->
-      <!-- Add the modifier "is-active" to display it on mobile -->
-      <div class="nav-right nav-menu">
-        <a class="nav-item">
-      <span class="icon"><i class="fa fa-github" aria-hidden="true"></i></span>
-      <span>github</span>    
-    </a>
-      </div>
-    </div>
-  </nav>
   <router-view></router-view>
   </div>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,17 @@
   <section class="hero is-light is-fullheight">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <img src="/static/img/mood.png" />       
         <div class="columns">
           <div class="column is-half is-offset-one-quarter">
+            <h1 class="title">
+              mood
+            </h1>
             <div class="field">
               <p class="control">
                 <input class="input" type="text" placeholder="@realdonaldtrump">
               </p>
             </div>
-            <a class="button is-primary is-outlined is-fullwidth">Get in the mood</a>
+            <a class="button is-primary is-fullwidth">Get in the mood</a>
           </div>
         </div>
       </div>
