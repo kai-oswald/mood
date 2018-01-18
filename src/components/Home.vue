a70bc9aafc71b19665ba00ad567b2071dc7cd02d
kai-oswald <kaos1910@gmail.com>
2018-01-18 16:44:35 +0100

    add logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,6 +10,7 @@
         <div class="container has-text-centered">
           <div class="columns">
             <div class="column is-half is-offset-one-quarter">
+            <img src="/static/img/mood.png"/>
               <h1 class="title">
                 mood
               </h1>
